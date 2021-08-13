095298aa407a64e5f4afd116e30b0cbdd2f302c8
Kalle <kalle@kalle.co>
2021-08-13 20:48:23 +0200

    lint and package

--- src/iracing.js
+++ src/iracing.js
@@ -1,5 +1,4 @@
 const fetch = require('node-fetch');
-const config = require('../config');
 
 const mapRace = (r) => {
   const obj = {
@@ -19,31 +18,31 @@ const mapRace = (r) => {
 };
 
 module.exports.getProfil = async (cookieString, irid) => fetch(`https://members.iracing.com/membersite/member/GetMember?memberId=${irid}`, {
-    method: 'GET',
-    headers: {
-      'Cookie': cookieString,
-    }
-  })
-  .then(res => {
-    if(res.ok && res.status == 200) return res.json();
+  method: 'GET',
+  headers: {
+    Cookie: cookieString,
+  },
+})
+  .then((res) => {
+    if (res.ok && res.status === 200) return res.json();
     throw new Error('can not get profile');
   });
 
-module.exports.getRaces = async (cookieString, irid, catId, year, season, week = '') => fetch(`https://members.iracing.com/memberstats/member/GetResults?custid=${irid}&showraces=1&showquals=0&showtts=0&showops=0&showofficial=1&showunofficial=0&showrookie=1&showclassd=1&showclassc=1&showclassb=1&showclassa=1&showpro=1&showprowc=1&lowerbound=0&upperbound=250&sort=start_time&order=desc&format=json&category=${catId}&seasonyear=${year}&seasonquarter=${season}&raceweek=${week}`, {
+module.exports.getRaces = async (cookieString, irid, catId, startTime, endTime) => fetch(`https://members.iracing.com/memberstats/member/GetResults?custid=${irid}&showraces=1&showquals=0&showtts=0&showops=0&showofficial=1&showunofficial=0&showrookie=1&showclassd=1&showclassc=1&showclassb=1&showclassa=1&showpro=1&showprowc=1&lowerbound=0&upperbound=250&sort=start_time&order=desc&format=json&category=${catId}&starttime_low=${startTime}&starttime_high=${endTime}`, {
   method: 'GET',
   headers: {
-    'Cookie': cookieString,
-  }
+    Cookie: cookieString,
+  },
 })
-.then(async (res) => {
-  if(res.ok && res.status == 200) return res.json();
-  console.log(`can not get races for ${irid}`);
-  let text = await res.text();
-  console.log(text);
-  throw new Error(`can not get races`);
-})
-.then(races => {
-  let rs = races.d.r;
-  if(!rs) return null;
-  return rs.map(mapRace);
-});
+  .then(async (res) => {
+    if (res.ok && res.status === 200) return res.json();
+    console.log(`can not get races for ${irid}`);
+    const text = await res.text();
+    console.log(text);
+    throw new Error('can not get races');
+  })
+  .then((races) => {
+    const rs = races.d.r;
+    if (!rs) return null;
+    return rs.map(mapRace);
+  });
--- src/results.js
+++ src/results.js
@@ -1,9 +1,12 @@
+/* eslint-disable prefer-destructuring */
+/* eslint-disable radix */
+/* eslint-disable no-console */
 const fetch = require('node-fetch');
 const neatCsv = require('neat-csv');
 
-//const config = require('../../config');
+// const config = require('../../config');
 
-/*const publishSubSessionToSns = async (subSession, subSessionId) => {
+/* const publishSubSessionToSns = async (subSession, subSessionId) => {
   const attr = {
     resource: {
       DataType: 'String',
@@ -19,14 +22,12 @@ const neatCsv = require('neat-csv');
     }
   };
 
-  
   const msg = {
     TopicArn: config.sns.arn,
     MessageAttributes: attr,
     Message: JSON.stringify(subSession),
   };
 
-  
   return sns.publish(msg).promise();
 }
 
@@ -36,10 +37,9 @@ const putResultToDb = async (result) => {
     Item: result,
   };
   return dynamodb.put(params).promise();
-};*/
+}; */
 
-
-/*const publishDriverResultToSns = async (subSession, subSessionId) => {
+/* const publishDriverResultToSns = async (subSession, subSessionId) => {
   const eventBody = result;
 
   const attr = {
@@ -57,77 +57,75 @@ const putResultToDb = async (result) => {
     }
   };
 
-  
   const msg = {
     TopicArn: config.sns.arn,
     Message: JSON.stringify(subSession),
     MessageAttributes: attr,
   };
 
-  
   return sns.publish(msg).promise();
-  
-}*/
+
+} */
 
 const levelSubLevelToLicense = (level, subLevel) => {
-    const intSubLevel = parseInt(subLevel);
-    const intLevel = parseInt(level);
-    // ProWc 28 7000
-    if (intLevel > 24) return 7000 + intSubLevel;
-    // Pro 24 6000
-    if (intLevel > 20) return 6000 + intSubLevel;
-    // A4 20 5000
-    if (intLevel > 16) return 5000 + intSubLevel;
-    // B4 16 4000
-    if (intLevel > 12) return 4000 + intSubLevel;
-    // C4 12 3000
-    if(intLevel > 8) return 3000 + intSubLevel;
-    // D4 8 2000
-    if(intLevel > 4) return 2000 + intSubLevel;
-    // R 4 1000
-    return 1000 + intSubLevel;
-}
+  const intSubLevel = parseInt(subLevel);
+  const intLevel = parseInt(level);
+  // ProWc 28 7000
+  if (intLevel > 24) return 7000 + intSubLevel;
+  // Pro 24 6000
+  if (intLevel > 20) return 6000 + intSubLevel;
+  // A4 20 5000
+  if (intLevel > 16) return 5000 + intSubLevel;
+  // B4 16 4000
+  if (intLevel > 12) return 4000 + intSubLevel;
+  // C4 12 3000
+  if (intLevel > 8) return 3000 + intSubLevel;
+  // D4 8 2000
+  if (intLevel > 4) return 2000 + intSubLevel;
+  // R 4 1000
+  return 1000 + intSubLevel;
+};
 
 const timeStringToMs = (ts) => {
-  if(ts == '' || ts.length > 9) return 0;
+  if (ts === '' || ts.length > 9) return 0;
   let time = 0;
   let split = ts.split(':');
-  if(split.length == 1) { // no minutes
+  if (split.length === 1) { // no minutes
     split = split[0];
   } else {
-    time += parseInt(split[0])*60*1000;
+    time += parseInt(split[0]) * 60 * 1000;
     split = split[1];
   }
   split = split.split('.');
-  time += parseInt(split[0])*1000 + parseInt(split[1]);
+  time += parseInt(split[0]) * 1000 + parseInt(split[1]);
   return time;
-}
+};
 
 const mapRaceCSVObj = (csvQbj) => ({
   rFinPos: parseInt(csvQbj['Fin Pos']),
   carId: parseInt(csvQbj['Car ID']),
-  carName: csvQbj['Car'],
+  carName: csvQbj.Car,
   carClassId: parseInt(csvQbj['Car Class ID']),
   carClassName: csvQbj['Car Class'],
   driverId: parseInt(csvQbj['Cust ID']),
   teamId: parseInt(csvQbj['Team ID']),
-  driverName: csvQbj['Name'],
+  driverName: csvQbj.Name,
   startPos: parseInt(csvQbj['Start Pos']),
   carNumber: parseInt(csvQbj['Car #']),
   outId: parseInt(csvQbj['Out ID']),
-  outText: csvQbj['Out'],
-  intervalText: csvQbj['Interval'],
+  outText: csvQbj.Out,
+  intervalText: csvQbj.Interval,
   lapsLed: parseInt(csvQbj['Laps Led']),
   fastestRaceLap: parseInt(csvQbj['Fast Lap#']) || 0,
   fastestRaceLapTime: timeStringToMs(csvQbj['Fastest Lap Time']),
   avgRaceLapTime: timeStringToMs(csvQbj['Average Lap Time']),
   completedRaceLaps: parseInt(csvQbj['Laps Comp']) || 0,
-  raceIncs: parseInt(csvQbj['Inc']),
-  points: parseInt(csvQbj['Pts']),
+  raceIncs: parseInt(csvQbj.Inc),
+  points: parseInt(csvQbj.Pts),
   clubPoints: parseInt(csvQbj['Club Pts']) || 0,
-  div: parseInt(csvQbj['Div']) || 0,
+  div: parseInt(csvQbj.Div) || 0,
   clubId: parseInt(csvQbj['Club ID']),
-  clubName: csvQbj['Club'],
+  clubName: csvQbj.Club,
   oldIRating: parseInt(csvQbj['Old iRating']) || 0,
   newIRating: parseInt(csvQbj['New iRating']) || 0,
   oldLicense: levelSubLevelToLicense(csvQbj['Old License Level'], csvQbj['Old License Sub-Level']),
@@ -150,36 +148,36 @@ const mapQualiToObj = (csvObj) => ({
 const getResult = async (subSessionId, cookieString, sessionType) => fetch(`https://members.iracing.com/membersite/member/GetEventResultsAsCSV?subsessionid=${subSessionId}&simsesnum=${sessionType}&includeSummary=0`, {
   method: 'GET',
   headers: {
-    'Cookie': cookieString,
-  }
+    Cookie: cookieString,
+  },
 })
-.then(async res => {
-  if(res.ok && res.url.includes('login')) {
-    const err = new Error('Cookie invalid');
-    logger.error(err);
-    err.type = 'cookie';
-    throw err;
-  }
-  if(res.ok) return res.text();
-  logger.info(`Res status is: ${res.status}`);
-  logger.info(`Cookiestring ${cookieString}`);
-  logger.info(`subSessionId ${subSessionId}`);
-  logger.info(`sessionType ${sessionType}`);
-  logger.info(`res text ${await res.text()}`);
-  logger.error(res);
-  const error =  new Error('Could not get results!?');
-  logger.error(error);
-  throw error;
-})
-.then(csv => {
-  if(csv.length < 450 && (csv.match(/\n/g) || '').length < 2) {
-    const error =  new Error('Result not found.');
-    error.type = 'notfound';
-    logger.error(error);
+  .then(async (res) => {
+    if (res.ok && res.url.includes('login')) {
+      const err = new Error('Cookie invalid');
+      console.log(err);
+      err.type = 'cookie';
+      throw err;
+    }
+    if (res.ok) return res.text();
+    console.log(`Res status is: ${res.status}`);
+    console.log(`Cookiestring ${cookieString}`);
+    console.log(`subSessionId ${subSessionId}`);
+    console.log(`sessionType ${sessionType}`);
+    console.log(`res text ${await res.text()}`);
+    console.log(res);
+    const error = new Error('Could not get results!?');
+    console.log(error);
     throw error;
-  }
-  return csv;
-});
+  })
+  .then((csv) => {
+    if (csv.length < 450 && (csv.match(/\n/g) || '').length < 2) {
+      const error = new Error('Result not found.');
+      error.type = 'notfound';
+      console.log(error);
+      throw error;
+    }
+    return csv;
+  });
 
 module.exports.getOfficalResults = async (cookieString, subSessionId) => {
   const metaData = {
@@ -197,49 +195,85 @@ module.exports.getOfficalResults = async (cookieString, subSessionId) => {
   // GET RACE Results
   const results = await getResult(subSessionId, cookieString, 0)
     .then(neatCsv)
-    .then(rawArray => {
+    .then((rawArray) => {
       const data = [];
       let ele;
-      for(let i = 0; i < rawArray.length; i++) {
+      for (let i = 0; i < rawArray.length; i += 1) {
         try {
           ele = mapRaceCSVObj(rawArray[i]);
-        } catch(err) {
-          logger.error('HERE', err);
+        } catch (err) {
+          console.log('HERE', err);
           throw err;
         }
 
-        if(i == 0) {
+        if (i === 0) {
           metaData.winnerName = ele.driverName;
           metaData.winnerId = ele.driverId;
         }
         // num drivers & mu,teams
-        if(ele.driverId < 0) {
-          metaData.numOfTeams++; // numOfTeams
-          if(!temp.teamData[`${ele.teamId}`]) temp.teamData[`${ele.teamId}`] = { sof: 0, ir: 0, drivers: [], name: ele.driverName };
-          if(!temp.classData[`${ele.carClassId}`]) temp.classData[`${ele.carClassId}`] = { ir: 0, classWinnerId: ele.driverId, classWinnerName: ele.driverName, finPos: 0, qPos: 0, driverCount: 0, sof: 0 };
-        } 
-        if(ele.driverId == ele.teamId)  {
-          if(!temp.classData[`${ele.carClassId}`]) temp.classData[`${ele.carClassId}`] = { ir: 0, classWinnerId: ele.driverId, classWinnerName: ele.driverName, finPos: 0, qPos: 0, driverCount: 0, sof: 0 };
-          temp.classData[`${ele.carClassId}`].finPos++;
+        if (ele.driverId < 0) {
+          metaData.numOfTeams += 1; // numOfTeams
+          if (!temp.teamData[`${ele.teamId}`]) {
+            temp.teamData[`${ele.teamId}`] = {
+              sof: 0, ir: 0, drivers: [], name: ele.driverName,
+            };
+          }
+          if (!temp.classData[`${ele.carClassId}`]) {
+            temp.classData[`${ele.carClassId}`] = {
+              ir: 0,
+              classWinnerId: ele.driverId,
+              classWinnerName: ele.driverName,
+              finPos: 0,
+              qPos: 0,
+              driverCount: 0,
+              sof: 0,
+            };
+          }
         }
-        if(ele.driverId > 0) {
-          metaData.numOfDrivers++; // numOfDrivers
+        if (ele.driverId === ele.teamId) {
+          if (!temp.classData[`${ele.carClassId}`]) {
+            temp.classData[`${ele.carClassId}`] = {
+              ir: 0,
+              classWinnerId: ele.driverId,
+              classWinnerName: ele.driverName,
+              finPos: 0,
+              qPos: 0,
+              driverCount: 0,
+              sof: 0,
+            };
+          }
+          temp.classData[`${ele.carClassId}`].finPos += 1;
+        }
+        if (ele.driverId > 0) {
+          metaData.numOfDrivers += 1; // numOfDrivers
           temp.driverIRSum += ele.oldIRating; // for sof
-          if(!temp.teamData[`${ele.teamId}`]) temp.teamData[`${ele.teamId}`] = {sof: 0, ir: 0, drivers: [], name: '' };
+          if (!temp.teamData[`${ele.teamId}`]) {
+            temp.teamData[`${ele.teamId}`] = {
+              sof: 0, ir: 0, drivers: [], name: '',
+            };
+          }
           temp.teamData[`${ele.teamId}`].ir += ele.oldIRating;
-          temp.teamData[`${ele.teamId}`].drivers.push({ 
+          temp.teamData[`${ele.teamId}`].drivers.push({
             driverName: ele.driverName,
             driverId: ele.driverId,
           });
           temp.teamData[`${ele.teamId}`].sof = Math.round(temp.teamData[`${ele.teamId}`].ir / temp.teamData[`${ele.teamId}`].drivers.length);
-          if(!temp.classData[`${ele.carClassId}`]) temp.classData[`${ele.carClassId}`] = { ir: 0, classWinnerId: ele.driverId, classWinnerName: ele.driverName, finPos: 0, qPos: 0, driverCount: 0, sof: 0 };
+          if (!temp.classData[`${ele.carClassId}`]) {
+            temp.classData[`${ele.carClassId}`] = {
+              ir: 0,
+              classWinnerId: ele.driverId,
+              classWinnerName: ele.driverName,
+              finPos: 0,
+              qPos: 0,
+              driverCount: 0,
+              sof: 0,
+            };
+          }
           temp.classData[`${ele.carClassId}`].ir += ele.oldIRating; // classSof
-          temp.classData[`${ele.carClassId}`].driverCount++;
+          temp.classData[`${ele.carClassId}`].driverCount += 1;
           temp.classData[`${ele.carClassId}`].sof = Math.round(temp.classData[`${ele.carClassId}`].ir / temp.classData[`${ele.carClassId}`].driverCount);
-
         }
         ele.classRFinPos = temp.classData[`${ele.carClassId}`].finPos;
-
 
         data.push(ele);
       }
@@ -255,18 +289,18 @@ module.exports.getOfficalResults = async (cookieString, subSessionId) => {
   try {
     qualiCSV = await getResult(subSessionId, cookieString, -1).then(neatCsv);
   } catch (err) {
-    logger.error(`Could not get Quali Setup for ${subSessionId}`);
+    console.log(`Could not get Quali Setup for ${subSessionId}`);
   }
 
   let enrichObj;
-  if(qualiCSV) {
+  if (qualiCSV) {
     // add qualidata
     enrichObj = {};
     let qEle;
-    for(let i = 0; i < qualiCSV.length; i++) {
+    for (let i = 0; i < qualiCSV.length; i += 1) {
       qEle = mapQualiToObj(qualiCSV[i]);
-      if(qEle.driverId == qEle.teamId)  {
-        temp.classData[`${qEle.carClassId}`].qPos++;
+      if (qEle.driverId === qEle.teamId) {
+        temp.classData[`${qEle.carClassId}`].qPos += 1;
       }
       qEle.classQFinPos = temp.classData[`${qEle.carClassId}`].qPos;
       enrichObj[qualiCSV[i]['Cust ID']] = qEle;
@@ -274,8 +308,8 @@ module.exports.getOfficalResults = async (cookieString, subSessionId) => {
   }
 
   // enrich and calculate
-  for( let i = 0; i < results.length; i++) {
-    if(enrichObj && enrichObj[`${results[i].driverId}`]) { 
+  for (let i = 0; i < results.length; i += 1) {
+    if (enrichObj && enrichObj[`${results[i].driverId}`]) {
       results[i].classQFinPos = enrichObj[`${results[i].driverId}`].classQFinPos;
       results[i].qFinPos = enrichObj[`${results[i].driverId}`].qFinPos;
       results[i].qualiyTime = enrichObj[`${results[i].driverId}`].qualiyTime;
@@ -283,12 +317,12 @@ module.exports.getOfficalResults = async (cookieString, subSessionId) => {
       results[i].fastestQualifyLap = enrichObj[`${results[i].driverId}`].fastestQualifyLap || 0;
       results[i].completedQualifyLaps = enrichObj[`${results[i].driverId}`].completedQualifyLaps;
     }
-    if(temp.teamData[`${results[i].teamId}`]) {
+    if (temp.teamData[`${results[i].teamId}`]) {
       results[i].teamName = temp.teamData[`${results[i].teamId}`].name;
       results[i].teamDrivers = temp.teamData[`${results[i].teamId}`].drivers;
       results[i].teamIR = temp.teamData[`${results[i].teamId}`].sof;
     }
-    if(temp.classData[`${results[i].carClassId}`]) {
+    if (temp.classData[`${results[i].carClassId}`]) {
       results[i].classWinnerId = temp.classData[`${results[i].carClassId}`].classWinnerId;
       results[i].classWinnerName = temp.classData[`${results[i].carClassId}`].classWinnerName;
       results[i].classSof = temp.classData[`${results[i].carClassId}`].sof;
@@ -296,185 +330,8 @@ module.exports.getOfficalResults = async (cookieString, subSessionId) => {
     }
   }
 
-
   return {
     metaData,
     results,
-  }
-}
-
-/*const getAndSaveOfficialResults = async ({ enrichData = {}, subSessionId, endTime = 0}, cookieString) => {
-  if(!subSessionId || subSessionId < 100000) {
-    const err = new Error(`Session ${subSessionId} seams invalid.`);
-    err.type = 'invalid';
-    throw err;
-  }
-  if(endTime && endTime > Date.now()) {
-    const err = new Error(`Session ${subSessionId} did not end yet. Reschedule...`);
-    err.type = 'notended';
-    throw err;
-  }
-  
-  // get results
-  // if type = notfound reschedule with tryInterval delay
-  // if type == cookie reschedule message with 15 min delay
-  const results = await getOfficalResults(cookieString, subSessionId); // TODO: 
-  const sessionJson = {
-    results: results.results,
-    catId: enrichData.catId,
-    endTime: enrichData.endTime,
-    seriesId: enrichData.seriesId,
-    seasionId: enrichData.seasionId,
-    splitNumber: enrichData.min ? subSessionId - enrichData.min + 1 : 0,
-    sessionId: enrichData.sessionId,
-    startTime: enrichData.startTime,
-    subSessionIds: enrichData.subSessionIds,
-    trackName: enrichData.trackName,
-    trackConfigName: enrichData.trackConfigName,
-    sof: results.metaData.sof,
-    numOfDrivers: results.metaData.numOfDrivers,
-    numOfTeams: results.metaData.numOfTeams,
-    winnerName: results.metaData.winnerName,
-    winnerId: results.metaData.winnerId,
-    scrapedAt: Date.now(),
   };
-
-  const zipped = await gzip(JSON.stringify(sessionJson));
-
-  const date = new Date();
-  
-  // build session file and save to s3
-  await s3.putObject({
-    Bucket: config.s3.bucketName,
-    Key: `${date.getUTCFullYear()}/${date.getUTCMonth()+1}/${date.getUTCDate()}/${subSessionId}.gzip`,
-    Body: zipped,
-  }).promise();
-
-  // publish to sns
-  try {
-    await publishSubSessionToSns(sessionJson, subSessionId);
-  } catch(err) {
-    logger.error(err, `Could not post ${subSessionId} to sns.`);
-    throw err;
-  }
-
-  // enrich results and save to dynamo
-  const dataArray = [];
-  const dynPro = sessionJson.results.map(r => {
-    const item = {
-      ...r,
-      ...enrichData,
-      sof: results.metaData.sof,
-      numOfDrivers: results.metaData.numOfDrivers,
-      numOfTeams: results.metaData.numOfTeams,
-      winnerName: results.metaData.winnerName,
-      splitNumber: enrichData.min ? subSessionId - enrichData.min + 1 : 0,
-      winnerId: results.metaData.winnerId,
-      scrapedAt: Date.now(),
-    };
-    dataArray.push(item);
-    return putResultToDb(item);
-  });
-
-  
-  try {
-    Promise.all(dynPro);
-  } catch(err) {
-    logger.error({ err, data: { subSessionId }}, `While putting result of ${subSessionId} to db.`);
-    throw err;
-  }
-
-  logger.info(`${dynPro.length} results saved to dyndb`);
-
-  return true;
-}
-
-module.exports.processSubsession = async ({subSessionId, seasionId, seriesId, subSessionIds, min, catId, endTime, sessionId, startTime, triesLeft = 2, trackName, trackConfigName}, cookieString) => {
-  
-  const enrichData = {
-    catId,
-    endTime,
-    sessionId,
-    startTime,
-    subSessionIds,
-    trackConfigName,
-    trackName,
-    subSessionId,
-    triesLeft,
-    seriesId,
-    seasionId,
-    min,
-  };
-  if (triesLeft <= 0) {
-    logger.info(enrichData, `Subsession has no more tries left: ${subSessionId}`);
-    await sqs.messageToDlQueue(enrichData);
-    return true;
-  }
-  if (!startTime) {
-    logger.info(enrichData, `Subsession message has no starttime: ${subSessionId}`);
-    await sqs.messageToDlQueue(enrichData);
-    return true;
-  }
-  if(!cookieString) {
-    logger.error({
-      enrichData,
-      err: new Error('No cookiestring in processSubsession'),
-    });
-    await sqs.messageToDlQueue(enrichData);
-    return true;
-  }
-  try {
-    await getAndSaveOfficialResults({ 
-      enrichData,
-      endTime,
-      subSessionId,
-    },
-    cookieString);
-  } catch(err) {
-    if (err.type === 'invalid') {
-      logger.info({
-        subSessionId,
-      },`SubSessionId seems invalid: ${subSessionId}`);
-      await sqs.messageToDlQueue(enrichData);
-      return true;
-    }
-    if (err.type === 'cookie') {
-      logger.error({
-        subSessionId,
-        cookieString,
-      },`Cookie was invalid while processing: ${subSessionId}`);
-      cookieHelper.removeCookie();
-      await sqs.rescheduleMessage(enrichData, 60*15);
-      return true;
-    }
-    if (err.type === 'notended') {
-      let delay = Math.round((endTime-Date.now())/1000)+5;
-      if(delay > 900) delay = 900;
-      if(delay < 60) delay = 60;
-      logger.info({
-        subSessionId,
-      }, `Reschedule ${subSessionId} as not ended yet.`);
-      await sqs.rescheduleMessage(enrichData, delay);
-      return true;
-    }
-    if (err.type === 'notfound') {
-      
-      enrichData.triesLeft--;
-      if(enrichData.triesLeft <= 0) {
-        await sqs.messageToDlQueue(enrichData);
-        logger.info({
-          subSessionId,
-        }, `Put ${subSessionId} to dl as results not available yet and no more tries.`);
-        return true;
-      }
-      logger.info({
-        subSessionId,
-      }, `Reschedule ${subSessionId} as results not available yet (${enrichData.triesLeft} tries left).`);
-      if(enrichData.triesLeft <= config.scrape.longIntervalStart) await sqs.rescheduleMessage(enrichData, config.scrape.tryIntervalLong);
-      else await sqs.rescheduleMessage(enrichData, config.scrape.tryInterval);
-      return true;
-    }
-    throw err;
-  }
-  return true;
-}*/
+};
